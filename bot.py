961f36a55e79d6c7fd2cdd297ac92163b1fc9d45
Mahdi0155 <t.mehdti21@gmail.com>
2025-05-03 12:04:09 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,6 @@ async def setup():
         listen="0.0.0.0",
         port=int(os.environ.get('PORT', 10000)),
         webhook_url=WEBHOOK_URL,
-        path="/webhook"
     )
 
 if __name__ == "__main__":
