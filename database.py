9ebfbfdaacdf9d2ef12aaeda7ba3a2ec82a54d31
Mahdi0155 <t.mehdti21@gmail.com>
2025-05-03 19:14:40 +0330

    Update database.py

--- database.py
+++ database.py
@@ -13,11 +13,9 @@ CREATE TABLE IF NOT EXISTS videos (
 """)
 conn.commit()
 
-def save_file(file_id):
-    code = str(abs(hash(file_id)))[0:8]
+def save_file(file_id, code):
     cur.execute("INSERT OR REPLACE INTO videos (code, file_id) VALUES (?, ?)", (code, file_id))
     conn.commit()
-    return code
 
 def get_file(code):
     cur.execute("SELECT file_id FROM videos WHERE code = ?", (code,))
