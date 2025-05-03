7b313e44d382fc656ef8f34ac3e0272ab3520fcc
Mahdi0155 <t.mehdti21@gmail.com>
2025-05-03 17:57:23 +0330

    Create database.py

---
+++ database.py
@@ -0,0 +1,25 @@
+# database.py
+
+import sqlite3
+
+conn = sqlite3.connect("videos.db", check_same_thread=False)
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS videos (
+    code TEXT PRIMARY KEY,
+    file_id TEXT NOT NULL
+)
+""")
+conn.commit()
+
+def save_file(file_id):
+    code = str(abs(hash(file_id)))[0:8]
+    cur.execute("INSERT OR REPLACE INTO videos (code, file_id) VALUES (?, ?)", (code, file_id))
+    conn.commit()
+    return code
+
+def get_file(code):
+    cur.execute("SELECT file_id FROM videos WHERE code = ?", (code,))
+    row = cur.fetchone()
+    return row[0] if row else None
