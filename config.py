650c3661daa4fa2125bf6d3c37bd53641c229a12
Mahdi0155 <t.mehdti21@gmail.com>
2025-05-03 18:04:16 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
-BOT_TOKEN = "توکن_ربات_اینجا"
-WEBHOOK_URL = "https://your-render-service.onrender.com/webhook"
-ADMIN_IDS = [123456789]  # شناسه عددی ادمین‌ها
+BOT_TOKEN = "7413532622:AAE2304MGwjtHqPUEFRRUXmlyq2bQAQqQFg"
+WEBHOOK_URL = "https://tttttt-v1kw.onrender.com/webhook"
+ADMIN_IDS = [6387942633]  # شناسه عددی ادمین‌ها
 CHANNEL_TAG = "@YourChannelTag"  # تگ کانال برای انتهای پست‌ها
 PING_INTERVAL = 240  # فاصله زمانی بین پینگ‌ها به ثانیه
